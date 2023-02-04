e9be48f18a850d0b70595b71934111960cc66543
Sergey Strakhov <x-ser@yandex.ru>
2023-02-04 16:27:17 +0300

    /low command now ask quantity of displayed elements Buttons added

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,5 +1,7 @@
 from telebot.types import Message
 from telebot import custom_filters
+
+from keyboards.inline.url_button import url_button
 from loader import bot
 from .goods_request import make_request
 from states.custom_states import MyStates
@@ -17,19 +19,35 @@ def low_command(message: Message) -> None:
 @bot.message_handler(state=MyStates.search)
 def get_search_result(message: Message) -> None:
     """
+    Получение результатов поиска, запрос количества отображаемых элементов
+    """
+    bot.set_state(message.from_user.id, MyStates.search_layout, message.chat.id)
+    bot.response = make_request(message.text)
+    bot.send_message(message.chat.id, "Сколько товаров отображать?\n(Максимум - 20)")
+
+
+@bot.message_handler(state=MyStates.search_layout)
+def get_result_layout(message: Message) -> None:
+    """
     Вывод результатов поиска
     """
-    response = make_request(message.text)
-    if response:
-        for item in response:
-            bot.send_photo(message.chat.id, f'http:{item["item"]["image"]}')
-            bot.send_message(message.chat.id,
-                             f'{item["item"]["title"]}\n'
-                             f'{item["item"]["sku"]["def"]["promotionPrice"]} руб.\n'
-                             f'https:{item["item"]["itemUrl"]}')
+    if message.text.isdigit() and 0 < int(message.text) < 21:
+        if bot.response:
+            counter = 1
+            for item in bot.response:
+                bot.send_photo(message.chat.id, f'http:{item["item"]["image"]}')
+                bot.send_message(message.chat.id,
+                                 f'{item["item"]["title"]}\n'
+                                 f'{item["item"]["sku"]["def"]["promotionPrice"]} руб.\n',
+                                 reply_markup=url_button(f'https:{item["item"]["itemUrl"]}'))
+                if counter == int(message.text):
+                    break
+                counter += 1
+        else:
+            bot.send_message(message.chat.id, "Что-то пошло не так")
+        bot.delete_state(message.from_user.id, message.chat.id)  # Сброс состояния
     else:
-        bot.send_message(message.chat.id, "Что-то пошло не так")
-    bot.delete_state(message.from_user.id, message.chat.id)  # Сброс состояния
+        bot.send_message(message.chat.id, "Введите число от 1 до 20")
 
 
 bot.add_custom_filter(custom_filters.StateFilter(bot))
--- states/custom_states.py
+++ states/custom_states.py
@@ -3,4 +3,5 @@ from telebot.handler_backends import State, StatesGroup
 
 class MyStates(StatesGroup):
     search = State()
+    search_layout = State()
     search_range = State()
